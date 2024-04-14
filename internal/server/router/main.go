9f4418fac7c4023ce8bede45607191a04ec5efe6
Paulo <git@pauloo27.dev>
2024-04-14 17:57:16 -0300

    feat: all the roads load to swagger

--- internal/server/router/main.go
+++ internal/server/router/main.go
@@ -1,21 +1,31 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/pauloo27/shurl/internal/server/api/health"
 	"github.com/pauloo27/shurl/internal/server/api/link"
-	"github.com/swaggo/http-swagger/v2"
+	httpSwagger "github.com/swaggo/http-swagger/v2"
+
 	// swagger :)
 	_ "github.com/pauloo27/shurl/internal/server/docs"
 )
 
 func RouteApp(root *chi.Mux) {
+	root.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/api/v1/swagger/index.html", http.StatusSeeOther)
+	})
+
 	root.Route("/api/v1", func(r chi.Router) {
 		r.Get("/healthz", health.Health)
 		r.Post("/links", link.Create)
 		r.Get("/swagger/*", httpSwagger.Handler(
 			httpSwagger.URL("/api/v1/swagger/doc.json"),
 		))
+		r.Get("/swagger", func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, "/api/v1/swagger/index.html", http.StatusSeeOther)
+		})
 	})
 
 	root.Get("/{slug}", link.Redirect)
